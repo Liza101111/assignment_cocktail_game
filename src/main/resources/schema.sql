f1fa73f7715297fd002c8c00fa54790e8f3d9be1
Liza <lixia101111@gmail.com>
2024-09-09 12:31:30 +0300

    format sql script

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,5 @@
-CREATE TABLE IF NOT EXISTS HighScore
-(
-    id
-    BIGINT
-    AUTO_INCREMENT
-    PRIMARY
-    KEY,
-    player_name
-    VARCHAR
-(
-    255
-) NOT NULL,
+CREATE TABLE IF NOT EXISTS HighScore (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    player_name VARCHAR(255) NOT NULL,
     score INT NOT NULL
-    );
+);
